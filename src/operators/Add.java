19c2aab24de297d010f57da212f60b14305257ef
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-10 22:10:54 +0530

    Problem_Solved

---
+++ src/operators/Add.java
@@ -0,0 +1,23 @@
+package operators;
+
+import java.util.Scanner;
+
+public class Add {
+
+    public static void main(String[] args) {
+
+        System.out.println("Taking Input from the user");
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter 1st no");
+        int a = sc.nextInt();
+
+        System.out.println("Enter 2nd no");
+        int b = sc.nextInt();
+
+        int sum = a + b;
+
+        System.out.println("Total sum is :" + sum);
+    }
+}
