692255c8666cf2db02a69562fe8692b2405da28a
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-14 19:40:56 +0530

    Leap_Year

---
+++ src/oops/Obj_Ref_Var.java
@@ -0,0 +1,16 @@
+package oops;
+
+public class Obj_Ref_Var {
+
+    String color;
+    int age;
+
+    public static void main(String[] args) {
+        Obj_Ref_Var blacky = new Obj_Ref_Var();
+
+        blacky.color = "Blacky";
+        blacky.age = 4;
+        System.out.println("Name : " + blacky.color + " " + "Age : "+blacky.age);
+
+    }
+}
