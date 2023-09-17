d29e18cc524be2a36702b9c02833dcb6e42dc79a
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-17 23:29:16 +0530

    Ptint 1 to 10

---
+++ src/problem_solved/Problem_11.java
@@ -0,0 +1,12 @@
+package problem_solved;
+
+public class Problem_11 {
+
+    public static void main(String[] args) {
+        System.out.println("Print 1 to 10");
+
+        for(int i =1 ; i<=10 ; i++){
+            System.out.println(i);
+        }
+    }
+}
