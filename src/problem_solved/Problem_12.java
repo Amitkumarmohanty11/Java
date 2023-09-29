dca08a9641d8318049e63f3ae80e1efa7b517606
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-29 21:28:07 +0530

    Problem-12

---
+++ src/problem_solved/Problem_12.java
@@ -0,0 +1,16 @@
+package problem_solved;
+
+import java.util.Scanner;
+
+public class Problem_12 {
+    public static void main(String[] args) {
+        System.out.println("Showing a message");
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter Your name ");
+
+        String name = sc.nextLine();
+
+        System.out.println("Hello " + name + " have a good day");
+    }
+}
