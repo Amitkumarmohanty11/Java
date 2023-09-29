e1c253888300c01134b6449a125e18e5ce95aa5a
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-29 21:33:36 +0530

    Problem-13

---
+++ src/problem_solved/Problem_14.java
@@ -0,0 +1,11 @@
+package problem_solved;
+
+public class Problem_14 {
+    public static void main(String[] args) {
+        System.out.println("Print 1 to 10");
+
+        for(int i =1 ; i<=10 ; i++){
+            System.out.println(i);
+        }
+    }
+}
