2dff137cc840819cc73b9b7d0898ea7dffe46384
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-09-29 21:26:11 +0530

    Problem-10

---
+++ src/problem_solved/Problem_10.java
@@ -0,0 +1,19 @@
+package problem_solved;
+
+public class Problem_10 {
+    public static void main(String[] args) {
+        for(int i =1 ; i<=4; i++){
+
+            for(int j=1; j<i ; j++) {
+                System.out.print("");
+            }
+            for(int k= i ; k <= 4; k++ ){
+                System.out.print("*");
+            }
+
+            System.out.println();
+        }
+    }
+
+
+}
