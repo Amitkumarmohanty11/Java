6ab5dae4a6a95ac8869308edac59e12fe36ea2b3
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-04 22:29:09 +0530

    Day_1

---
+++ src/psa_java_day_1/Animal1.java
@@ -0,0 +1,11 @@
+package psa_java_day_1;
+
+public class Animal1 {
+    public static void main(String[] args){
+        Animal1 buzo = new Animal1();
+        Animal1 b1 = new Animal1();
+
+        System.out.println(buzo);
+        System.out.println(b1);
+    }
+}
