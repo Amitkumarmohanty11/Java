5cd40831ab7594962bd372a76a7ec7eaa3f0d9c5
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-04 22:28:55 +0530

    Day_1

---
+++ src/psa_java_day_1/Butu.java
@@ -0,0 +1,13 @@
+package psa_java_day_1;
+
+public class Butu {
+    public static void main(String[] args){
+        Butu b2 = new Butu();
+        Butu b4 = new Butu();
+        Butu b7 = new Butu();
+
+        System.out.println(b2);
+        System.out.println(b4);
+        System.out.println(b7);
+    }
+}
