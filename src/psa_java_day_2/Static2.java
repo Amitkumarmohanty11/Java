77a1ff2f013872bd84c4934b5d59da8ccb8d3620
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-05 22:11:22 +0530

    Day_2

---
+++ src/psa_java_day_2/Static2.java
@@ -0,0 +1,11 @@
+package psa_java_day_2;
+
+public class Static2 {
+    static int x = 20;
+
+    public static void main(String[] args) {
+        System.out.println(Static2.x);
+
+        System.out.println(x);
+    }
+}
