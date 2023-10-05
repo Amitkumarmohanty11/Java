5b2667eaf2b289db9c819e973d4a6a580e2fcb85
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-05 22:10:06 +0530

    Day_2

---
+++ src/psa_java_day_2/Non_Static1.java
@@ -0,0 +1,12 @@
+package psa_java_day_2;
+
+public class Non_Static1 {
+
+    int x = 10;
+
+    public static void main(String[] args) {
+        Non_Static1 n1 = new Non_Static1();
+
+        System.out.println(n1.x);
+    }
+}
