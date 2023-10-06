9b4d072efd3d965dc578c534d4bc4114d6a7900d
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-06 21:09:33 +0530

    Day_3(Methods)

---
+++ src/psa_java_day_3/Method3.java
@@ -0,0 +1,11 @@
+package psa_java_day_3;
+
+public class Method3 {
+    public static void main(String[] args) {
+        Method3.test();
+    }
+
+    public static void test(){
+        System.out.print("amit kumar");
+    }
+}
