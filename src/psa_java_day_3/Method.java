d94ef4f9580b523bafd6d2b6382c4f5d69a1f40a
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-06 21:08:50 +0530

    Day_3(Methods)

---
+++ src/psa_java_day_3/Method.java
@@ -0,0 +1,11 @@
+package psa_java_day_3;
+
+public class Method {
+    public static void main(String[] args) {
+
+    }
+    public void test(){
+        System.out.println(100);
+    }
+}
+//Process finished with exit code 0
