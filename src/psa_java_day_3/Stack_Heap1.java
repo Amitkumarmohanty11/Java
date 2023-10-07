233086cf46518535701f5d58e1da7df12a9ba5fc
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-07 20:42:31 +0530

    Day_3(Stack and heap)

---
+++ src/psa_java_day_3/Stack_Heap1.java
@@ -0,0 +1,13 @@
+package psa_java_day_3;
+
+public class Stack_Heap1 {
+    int a = 10;
+    public static void main(String[] args) {
+        Stack_Heap1 a1 = new Stack_Heap1();
+
+
+
+        System.out.println(a1.a);
+
+    }
+}
