9b101af7a8471f19bd8380622ba14d93a5af3fa7
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-08 22:06:04 +0530

    Day_3(Stack and heap)

---
+++ src/psa_java_day_3/Stack_Heap2.java
@@ -0,0 +1,14 @@
+package psa_java_day_3;
+
+public class Stack_Heap2 {
+
+    public static void main(String[] args) {
+        String a = "Amit";
+        Stack_Heap2 a1 = new Stack_Heap2();
+
+    }
+    public void xyz(){
+
+        System.out.println(a1.a);
+    }
+}
