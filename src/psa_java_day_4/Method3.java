47a89c43e76dd7e57beab9f37e5e4f8e63d6ad36
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-09 20:27:15 +0530

    Day_4(Methods)

---
+++ src/psa_java_day_4/Method3.java
@@ -0,0 +1,15 @@
+package psa_java_day_4;
+
+public class Method3 {
+    public static void main(String[] args) {
+        Method2 a1 = new Method2();
+        a1.test();
+        a1.tes2();
+    }
+    public void test(){
+        System.out.println(100);
+    }
+    public void tes2(){
+        System.out.println(200);
+    }
+}
