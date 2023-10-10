441813da1fb4b02a8db9c1be95b65bd51e118bbb
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-10 23:38:58 +0530

    Day_5

---
+++ src/psa_java_day_5/Post_Inc1.java
@@ -0,0 +1,11 @@
+package psa_java_day_5;
+
+public class Post_Inc1 {
+    public static void main(String[] args) {
+        int i = 10;
+        int j = i++ + i +  i++ + i++ ;
+
+        System.out.println(i);
+        System.out.println(j);
+    }
+}
