0b8d9b25030d360da4573215f1e04affd5e6331b
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-13 23:10:21 +0530

    Day_6

---
+++ src/psa_java_day_6/Auto_up_casting1.java
@@ -0,0 +1,9 @@
+package psa_java_day_6;
+
+public class Auto_up_casting1 {
+    public static void main(String[] args) {
+        long i = 10;
+        //byte j = i;
+        //System.out.println(j);
+    }
+}//O/P = Error
