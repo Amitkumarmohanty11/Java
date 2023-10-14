c6310590df9e75b4cef3efd15b6c99ff1c394ee7
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-14 23:47:55 +0530

    Day_6

---
+++ src/psa_java_day_6/Reference_Variable1.java
@@ -0,0 +1,9 @@
+package psa_java_day_6;
+
+public class Reference_Variable1 {
+    public static void main(String[] args) {
+        Reference_Variable a1 =new Reference_Variable();
+        System.out.println(a1);
+
+    }
+}
