78c95a778d39a489a87c77b376b214aeb5f7b5eb
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-17 22:28:23 +0530

    Day_7(Methods)

--- src/psa_java_day_7/Method2.java
+++ src/psa_java_day_7/Method2.java
@@ -2,8 +2,8 @@ package psa_java_day_7;
 
 public class Method2 {
     public static void main(String[] args) {
-        Method2 b1 = new test();
-        b1.test();
+        //Method2 b1 = new test();
+       // b1.test();
     }
     public void test(){
         System.out.println(100);
