142db631619bd4723c898ec5d1ca32d34cae5a60
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-17 22:09:20 +0530

    Day_7(Methods)

---
+++ src/psa_java_day_7/Method1.java
@@ -0,0 +1,13 @@
+package psa_java_day_7;
+
+//If a methods is void then that method can not return any value
+public class Method1 {
+    public static void main(String[] args) {
+
+    }
+    public void test(){//non static
+        //return 100;
+    }
+}
+//O/P = Error
+
