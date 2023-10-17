64ffa2be32a8abee658bf620008cbb9b085d0a5d
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-17 22:29:14 +0530

    Day_7(return keyword)

---
+++ src/psa_java_day_7/Return2.java
@@ -0,0 +1,15 @@
+package psa_java_day_7;
+//return
+//if we write anything after return keyword then that code will never run , hence we will get unreachable code error
+//Example
+public class Return2 {
+    public static void main(String[] args) {
+        Return2 a1= new Return2();
+                a1.test();
+    }
+    public void test(){
+        return;
+        System.out.println(100);
+    }
+}
+//O/P == unreachable statement
