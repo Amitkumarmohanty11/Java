5516cc4ad23dbbd6ea96bcd1c21d4366948a9951
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-17 22:29:01 +0530

    Day_7(return keyword)

---
+++ src/psa_java_day_7/Return1.java
@@ -0,0 +1,15 @@
+package psa_java_day_7;
+//return
+// It can be used inside void methods
+//Example
+public class Return1 {
+    public static void main(String[] args) {
+        Return1 a1=new Return1();
+        a1.test();
+
+    }
+    public void test(){
+        System.out.println(100);
+        return;
+    }
+}
