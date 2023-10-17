8578b8876f7358704565e31f8916ac09434e52f8
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-17 22:17:53 +0530

    Day_7(Methods)

---
+++ src/psa_java_day_7/Method2.java
@@ -0,0 +1,12 @@
+package psa_java_day_7;
+
+public class Method2 {
+    public static void main(String[] args) {
+        Method2 b1 = new test();
+        b1.test();
+    }
+    public void test(){
+        System.out.println(100);
+    }
+}
+//O/P = Error
