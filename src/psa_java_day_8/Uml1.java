04e802dbce53066dede6a9f9d6fefcbb09a957d1
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-18 19:02:04 +0530

    Day_8(UML)

---
+++ src/psa_java_day_8/Uml1.java
@@ -0,0 +1,14 @@
+package psa_java_day_8;
+
+public class Uml1 {
+    public static void main(String[] args) {
+        Uml1 a=new Uml1();
+        a.test(10,"Amit");
+    }
+    public void test(int x, String y){
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
+//O/p = 10
+//Amit
