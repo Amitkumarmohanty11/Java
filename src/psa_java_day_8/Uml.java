568e553a2fc9f855c3329c1ffa4fb8e5fbb774d7
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-18 19:01:52 +0530

    Day_8(UML)

---
+++ src/psa_java_day_8/Uml.java
@@ -0,0 +1,12 @@
+package psa_java_day_8;
+
+public class Uml {
+    public static void main(String[] args) {
+        Uml a= new Uml();
+        a.test(500);
+    }
+    public void test(int x){
+        System.out.println(x);
+    }
+}
+//o/p=500
