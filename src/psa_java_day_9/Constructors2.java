8ddb0037c2db88f5382ecbf8635c3b3ef6fa2f19
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-19 22:50:49 +0530

    Day_9(Constructors)

---
+++ src/psa_java_day_9/Constructors2.java
@@ -0,0 +1,16 @@
+package psa_java_day_9;
+//point-3
+// Constructors are internally void and can never return any value
+//Example
+
+public class Constructors2 {
+    Constructors2(){
+        //return 100; _______we can not  pass the  return  value.
+
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+//O/P == unexpected return value (Error)
