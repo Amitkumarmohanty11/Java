ee258f2eee59b780b2ed682c4059500e96918866
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-19 22:51:25 +0530

    Day_9(Constructors)

---
+++ src/psa_java_day_9/Constructors_Arg.java
@@ -0,0 +1,13 @@
+package psa_java_day_9;
+
+public class Constructors_Arg {
+    Constructors_Arg(int x){
+        System.out.println(x);
+
+    }
+
+    public static void main(String[] args) {
+        Constructors_Arg a=new Constructors_Arg(100);
+
+    }
+}
