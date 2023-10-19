d65be09562b4a4650eaaa32ccae6d7e54c4205d5
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-19 22:50:07 +0530

    Day_9(Constructors)

---
+++ src/psa_java_day_9/Constructors.java
@@ -0,0 +1,15 @@
+package psa_java_day_9;
+//Constructors
+//note..
+// point -1 --Constructors should have same name as that of class
+//point -2 Whenever we create object Constructors are called
+
+// Example / point -1
+
+public class Constructors {
+    Constructors(){
+        System.out.println(100);
+    }
+
+
+}
