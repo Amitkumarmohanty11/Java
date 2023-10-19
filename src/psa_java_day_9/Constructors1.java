a61d6c3441bc6fbfa8b688b2e75ec97cd9dcbe6f
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-19 22:50:39 +0530

    Day_9(Constructors)

---
+++ src/psa_java_day_9/Constructors1.java
@@ -0,0 +1,14 @@
+package psa_java_day_9;
+//point -2 --Whenever we create object Constructors are called
+public class Constructors1 {
+    Constructors1(){
+
+        System.out.println(1000);
+    }
+
+    public static void main(String[] args) {
+        Constructors1 a1=new Constructors1();
+        Constructors1 a2=new Constructors1();
+
+    }
+}
