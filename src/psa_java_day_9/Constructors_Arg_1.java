63521d02ef405460f1922503a0819c38f39a44fb
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-20 22:46:47 +0530

    Day_9(Constructors)

---
+++ src/psa_java_day_9/Constructors_Arg_1.java
@@ -0,0 +1,20 @@
+package psa_java_day_9;
+
+// note
+// By default numbers  in java are treated as integer datatype , Hence below program throws error
+
+//Example
+
+public class Constructors_Arg_1 {
+
+    Constructors_Arg_1(byte x){
+        System.out.println(x);
+    }
+
+    public static void main(String[] args) {
+        //Constructors_Arg_1 a = new Constructors_Arg_1(100);
+    }
+
+}
+
+//O/P == Error (  prob Solu in  Constructors_Arg_2 just check )
