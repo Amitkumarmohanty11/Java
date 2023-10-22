12212762b319bc240a99a94da14d9451f2e98865
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-22 23:01:17 +0530

    IIB

---
+++ src/iib/Iib_4.java
@@ -0,0 +1,18 @@
+package iib;
+
+public class Iib_4 {
+    {//IIB
+        System.out.println("From IIB");
+    }
+    Iib_4(){//Constructor
+        System.out.println("From Constructor");
+    }
+
+    public static void main(String[] args) {
+        Iib_4 a1 = new Iib_4();
+        System.out.println("From main");
+    }
+}
+//O/P ==  From IIB
+//From Constructor
+//From main
