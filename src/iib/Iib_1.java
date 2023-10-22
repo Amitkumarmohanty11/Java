33b2ca30de192a0de35b4e3417b80e8eb1a824a8
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-22 23:00:38 +0530

    IIB

---
+++ src/iib/Iib_1.java
@@ -0,0 +1,12 @@
+package iib;
+
+public class Iib_1 {
+    {
+        System.out.println("From IIB");
+    }
+
+    public static void main(String[] args) {
+        Iib_1 a1 = new Iib_1();
+    }
+}
+// O/P == From IIB
