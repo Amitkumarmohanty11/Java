f7ffd1ea722dfec6e9daf36cd9ae9c14161b0a60
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-22 23:01:51 +0530

    IIB

---
+++ src/iib/Iib_6.java
@@ -0,0 +1,14 @@
+package iib;
+
+public class Iib_6 {
+    int i;
+    {
+        i=10;
+        System.out.println(i);
+    }
+
+    public static void main(String[] args) {
+        Iib_6 a1=new Iib_6();
+    }
+}
+//O/P == 10
