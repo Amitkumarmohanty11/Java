17cea0982cb34be20684aa4b3996ce8efd2f42d5
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-23 23:31:52 +0530

    SIB

---
+++ src/sib/Sib_1.java
@@ -0,0 +1,14 @@
+package sib;
+
+public class Sib_1 {
+    static
+    {
+        System.out.println("From SIB");
+    }
+
+    public static void main(String[] args) {
+        System.out.println("From main");
+    }
+}
+//O/P == From SIB
+//From main
