abb9cebd07b041d755d2c9eebd2df25400be75ac
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-23 23:29:58 +0530

    SIB

---
+++ src/sib/Sib.java
@@ -0,0 +1,16 @@
+package sib;
+//SIB runs before main method and it does not required any INVOKING statement
+
+//Example:-
+
+public class Sib {
+    static
+    {
+        System.out.println("From SIB");
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+//O/P = From SIB
