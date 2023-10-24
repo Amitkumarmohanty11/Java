746a3cd4a7ea492787e37d4bea6cf8bf9c1fc396
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-24 21:45:41 +0530

    Operators

---
+++ src/operators/Mul.java
@@ -0,0 +1,10 @@
+package operators;
+
+public class Mul {
+    public static void main(String[] args) {
+        int a=60;
+        int b=20;
+        int x = a*b;
+        System.out.println(x);
+    }
+}
