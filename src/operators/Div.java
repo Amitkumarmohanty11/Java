114297afe16a4ec456172b52ee43ce0fc72e9076
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-24 21:45:56 +0530

    Operators

---
+++ src/operators/Div.java
@@ -0,0 +1,12 @@
+package operators;
+
+public class Div {
+    public static void main(String[] args) {
+        int a=60;
+        int b=20;
+        int x = a/b;
+        int y = a%b;
+        System.out.println(x);
+        System.out.println(y);
+    }
+}
