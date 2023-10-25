600eb1c1f56264d84900884c25b58fdad261980d
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-25 22:46:48 +0530

    Day-11

---
+++ src/psa_java_day_11/This_non_static_member1.java
@@ -0,0 +1,14 @@
+package psa_java_day_11;
+
+public class This_non_static_member1 {
+//    This_non_static_member1(int x){
+//        System.out.println(x);
+//    }
+//    This_non_static_member1() {
+//        this();
+//    }
+//
+//    public static void main(String[] args) {
+//        This_non_static_member1 a1 = new This_non_static_member1();
+//    }
+}
