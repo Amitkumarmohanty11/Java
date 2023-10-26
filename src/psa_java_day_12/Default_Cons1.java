e886859e8b155c4e0b8f860846540622370562c0
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-26 23:55:17 +0530

    Day-12

---
+++ src/psa_java_day_12/Default_Cons1.java
@@ -0,0 +1,11 @@
+package psa_java_day_12;
+
+public class Default_Cons1 {
+    Default_Cons1(){
+        System.out.println("Amit");
+    }
+
+    public static void main(String[] args) {
+        Default_Cons1 a1 =new Default_Cons1();
+    }
+}
