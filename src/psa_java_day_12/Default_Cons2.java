0b6a28a0bd51ee9111dc2e8c19a416bd644083e5
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-27 00:04:50 +0530

    Day-12

---
+++ src/psa_java_day_12/Default_Cons2.java
@@ -0,0 +1,8 @@
+package psa_java_day_12;
+//compiler automaticaly create constructor
+public class Default_Cons2 {
+
+    public static void main(String[] args) {
+        Default_Cons2 a1 =new Default_Cons2();
+    }
+}
