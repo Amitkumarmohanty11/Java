665ba4dfbb6f76f181165aef03c48fdb15e60825
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-27 22:55:45 +0530

    Day-12

---
+++ src/iib_sib_con_methods/A.java
@@ -0,0 +1,16 @@
+package iib_sib_con_methods;
+
+public class A {
+    {
+        System.out.println("IIB");
+    }
+    static
+    {
+        System.out.println("SIB");// 1st output;
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
+//O/P == SIB
