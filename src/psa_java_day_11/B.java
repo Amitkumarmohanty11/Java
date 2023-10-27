07630bc31e8e4fc0ab1e4dfa5656d617cbbe539e
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-27 22:40:11 +0530

    Day-12

--- src/psa_java_day_11/B.java
+++ src/psa_java_day_11/B.java
@@ -3,14 +3,14 @@ package psa_java_day_11;
 //When we call one constructor from another constructor it is called constructor chaining;
 public class B {
 
-    B(int x){
-        System.out.println(x);
-    }
-    B(){
-        this();
-    }
-
-    public static void main(String[] args) {
-        B b1=new B();
-    }
+//    B(int x){
+//        System.out.println(x);
+//    }
+//    B(){
+//        this();
+//    }
+//
+//    public static void main(String[] args) {
+//        B b1=new B();
+//    }
 }
