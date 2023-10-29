ea4a1607694d8e76a930f2b2ab8391bedc87acd4
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-29 23:39:08 +0530

    Day-13

---
+++ src/psa_java_day_13/Animal.java
@@ -0,0 +1,14 @@
+package psa_java_day_13;
+
+public class Animal {
+    public void sleep() {
+        System.out.println("sleeping");
+    }
+
+    public void eat() {
+        System.out.println("Eating");
+    }
+
+
+
+}
