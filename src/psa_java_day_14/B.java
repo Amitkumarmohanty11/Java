3565b5c0f09f047246c012200cea4b056db1c4eb
Amitkumarmohanty11 <mohantyamitkumar84@gmail.com>
2023-10-31 23:44:11 +0530

    Day-14

---
+++ src/psa_java_day_14/B.java
@@ -0,0 +1,11 @@
+package psa_java_day_14;
+
+public class B extends A{
+    public static void main(String[] args) {
+
+        B b1= new B();
+        //System.out.println(b1.x);//Error
+        //b1.test();//Error
+    }
+}
+//Error
