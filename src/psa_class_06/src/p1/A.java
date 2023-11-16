5c09d4fb346aef7f8308b591ad332225efffa30d
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2023-11-16 23:23:28 +0530

    Add files via upload

---
+++ src/psa_class_06/src/p1/A.java
@@ -0,0 +1,13 @@
+package p1;
+
+// Reference variable in java.
+public class A {
+	
+	public static void main(String[] args) {
+		
+		A a1 = null;
+		
+		System.out.println(a1);
+	}
+
+}
---
+++ src/psa_class_06/src/p1/B.java
@@ -0,0 +1,16 @@
+package p1;
+
+public class B {
+	
+	public static void main(String[] args) {
+		
+		B b1 =  new B();
+		
+		B b2 =  new B();
+		
+		System.out.println(b1);
+		
+		System.out.println(b2);
+	}
+
+}
