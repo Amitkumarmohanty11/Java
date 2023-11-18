3e8dc8e45f8a7f610e3e09df93ac063d48b3ae6a
Amit Kumar Mohanty <105871364+Amitkumarmohanty11@users.noreply.github.com>
2023-11-18 23:50:51 +0530

    Add files via upload

---
+++ unary_operator/src/p1/H.java
@@ -0,0 +1,14 @@
+package p1;
+
+public class H {
+	
+	public static void main(String[] args) {
+		 
+		int i = 10;
+		int j = ++i + i + ++i + ++i + i-- + --i + i ;
+		System.out.println(i);
+		
+		System.out.println(j);
+	}
+
+}
